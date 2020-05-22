3e07dcc7dcfb285b8977c09cf94aa659181fd56d
Louis Grey <tuantranquang20@gmail.com>
2020-05-22 17:55:19 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import store from "./redux/store";
+import { Provider } from "react-redux";
+import AppNavigator from './navigation/AppNavigator';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <AppNavigator />
+    </Provider>
+  );
+}
+export default App;
